3d63fb9766b07e4cb04f0811d2a2483d4132715f
therockspush <therockspush@users.noreply.github.com>
2022-08-01 09:54:57 -0700

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aviatrix" {
+  username                = "admin"
+  password                = var.password
+  controller_ip           = ""
+  skip_version_validation = true
+  version                 = "2.20.3"
+  
+  
+
+  
+}
